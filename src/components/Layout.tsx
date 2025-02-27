ebed5b8e90cfd924e6432949f3e4c77a3ef2b868
DatBui305 <buianhdat305@gmail.com>
2025-02-27 15:35:21 +0700

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,13 @@
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-
+import { Toaster } from "sonner";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen w-full overflow-hidden">
       {" "}
       {/* Thêm overflow-hidden */}
       {/* Sidebar */}
+      <Toaster richColors position="top-right" />
       <Sidebar />
       {/* Main Content */}
       <div className="flex flex-col flex-1">
